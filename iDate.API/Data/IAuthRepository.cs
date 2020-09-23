9f16bafa84d3835cf1ade774f78d4a86f13ee17b
Joshua Davis <53221972+josh-davis-git@users.noreply.github.com>
2020-09-23 10:31:35 -0400

    completed ef work
    next -- controllers, helpers, dating repo + i dating repo

---
+++ iDate.API/Data/IAuthRepository.cs
@@ -0,0 +1,12 @@
+using System.Threading.Tasks;
+using iDate.API.Models;
+
+namespace iDate.API.Data
+{
+    public interface IAuthRepository
+    {
+        Task<User> Register(User user, string password);
+        Task<User> Login(string username, string password);
+        Task<bool> UserExists(string username);
+    }
+}
